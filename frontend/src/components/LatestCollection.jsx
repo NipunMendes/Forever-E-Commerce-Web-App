44d844d336882908f29a98522aa06d7833c11ea0
Nipun Mendes <nipunmendes@gmail.com>
2024-10-22 11:23:38 +0530

    Home page frontend done
    Navbar to Footer done

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -17,7 +17,7 @@ const LatestCollection = () => {
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
         <Title text1={'LATEST'} text2={'COLLECTIONS'} />
-        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit
         </p>
       </div>
 
